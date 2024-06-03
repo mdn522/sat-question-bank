1d0e492f6bb3dea3cfe04323fae82a446d128ce7
ABDULLAH-LAPTOP\mdn522 <mdn522@gmail.com>
2024-06-04 00:09:37 +0600

    Overhauled logics to add filter Added Difficulty and Bluebook FIlter Recolored

--- app.py
+++ app.py
@@ -1,110 +1,245 @@
 from collections import defaultdict
-from typing import List
+from functools import partial
+from typing import List, Tuple
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 import json
 from pathlib import Path
 from diskcache import Index
 
+import core
+from core import get_categories, get_questions_id, get_questions_count
+
 app = Flask(__name__)
 
-db_path = Path(__file__).parent / Path(r"data/cb-digital-questions.json")
+data_path = Path(__file__).parent / 'data'
+db_path = data_path / r"cb-digital-questions.json"
 with db_path.open(encoding='utf-8') as fp:
     questions = json.load(fp)
 
+with (data_path / r"lookup.json").open(encoding='utf-8') as fp:
+    filter_data = json.load(fp)
+
+    for k, v in [('mathLiveItems', 'math'), ('readingLiveItems', 'english')]:
+        filter_data[v] = filter_data[k]
+        del filter_data[k]
+
 
 question_id_map = {}
-categories = {}
-categories_questions_id = defaultdict(list)
-categories_counts = defaultdict(int)
-categories_first_question_id = {}
+# categories = {}
+# categories_questions_id = defaultdict(list)
+# categories_counts = defaultdict(int)
+# categories_first_question_id = {}
 module_count = defaultdict(int)
+terms = {}
+
 
 for q_id, q in questions.items():
     question_id_map[q['questionId']] = q_id
 
 
+for q_id, q_meta in questions.items():
+    if q_meta['module'] not in terms:
+        terms[q_meta['module']] = q_meta['module'].title()
+
+    if q_meta['primary_class_cd'] not in terms:
+        terms[q_meta['primary_class_cd']] = q_meta['primary_class_cd_desc']
+
+    if q_meta['skill_cd'] not in terms:
+        terms[q_meta['skill_cd']] = q_meta['skill_desc']
+
+    if q_meta['difficulty'] not in terms:
+        terms[q_meta['difficulty']] = {'E': 'Easy', 'M': 'Medium', 'H': 'Hard'}[q_meta['difficulty']]
+
+
 def load_categories():
     global categories
 
+    # q_filter_cd = {
+    #     'all': 'A',
+    #     'bluebook': 'BO',
+    #     'non': 'NB',
+    # }
+
     for q_id, q_meta in questions.items():
-        if q_meta['primary_class_cd'] not in categories:
-            categories[q_meta['primary_class_cd']] = {
-                'id': q_meta['primary_class_cd'],
-                'name': q_meta['primary_class_cd_desc'],
-                'count': 0,
-                'categories': {},
-                'questions_id': [],
-                'module': q_meta['module'],
-            }
+        # q_difficulty = q_meta['difficulty']
+        # q_active = ['all']
+        # q_active.append('bluebook-only' if q_id in filter_data[q_meta['module']] else 'non-bluebook')
 
-        if q_meta['skill_cd'] not in categories[q_meta['primary_class_cd']]['categories']:
-            categories[q_meta['primary_class_cd']]['categories'][q_meta['skill_cd']] = {
-                'id': q_meta['skill_cd'],
-                'name': q_meta['skill_desc'],
-                'count': 0,
-                'questions_id': []
-            }
+        # question keys
+        # module -> class -> skill -> difficulty -> active
 
-        categories[q_meta['primary_class_cd']]['count'] += 1
-        categories[q_meta['primary_class_cd']]['categories'][q_meta['skill_cd']]['count'] += 1
+        # keys = {
+        #     'module': q_meta['module'][0].upper(),
+        #     'difficulty': q_difficulty,
+        #     'class': q_meta['primary_class_cd'],
+        #     'skill': q_meta['skill_cd'],
+        #     'active': ['all'] + ['bluebook-only' if q_id in filter_data[q_meta['module']] else 'non-bluebook']
+        # }
+        #
+        # key_module = q_meta['module'][0].upper()
+        # key_module__difficulty = key_module + '-' + q_difficulty
+        # key_class = key_module + '-' + q_meta['primary_class_cd']
+        # key_class__difficulty = key_class + '-' + q_difficulty
+        # key_skill = key_class + '-' + q_meta['skill_cd']
+        # key_skill__difficulty = key_skill + '-' + q_difficulty
+        # keys = [
+        #     ['module'],
+        #     ['module', 'difficulty'],
+        #     ['module', 'difficulty', 'active'],
+        #     ['module', 'active'],
+        # ]
 
-        categories_questions_id[q_meta['primary_class_cd']].append([q_id, q_meta['questionId']])
-        categories_counts[q_meta['primary_class_cd']] += 1
-        categories_questions_id[q_meta['primary_class_cd'] + '-' + q_meta['skill_cd']].append([q_id, q_meta['questionId']])
-        categories_counts[q_meta['primary_class_cd'] + '-' + q_meta['skill_cd']] += 1
-        if q_meta['primary_class_cd'] not in categories_first_question_id:
-            categories_first_question_id[q_meta['primary_class_cd']] = q_meta['questionId']
-
-        module_count[q_meta['module']] += 1
-
-        if (q_meta['primary_class_cd'] + '-' + q_meta['skill_cd']) not in categories_first_question_id:
-            categories_first_question_id[q_meta['primary_class_cd'] + '-' + q_meta['skill_cd']] = q_meta['questionId']
+        # Maybe legacy code
+        # if q_meta['primary_class_cd'] not in categories:
+        #     categories[q_meta['primary_class_cd']] = {
+        #         'id': q_meta['primary_class_cd'],
+        #         'name': q_meta['primary_class_cd_desc'],
+        #         'count': 0,
+        #         'categories': {},
+        #         'questions_id': [],
+        #         'module': q_meta['module'],
+        #     }
+        #
+        # # Legacy code
+        # if q_meta['skill_cd'] not in categories[q_meta['primary_class_cd']]['categories']:
+        #     categories[q_meta['primary_class_cd']]['categories'][q_meta['skill_cd']] = {
+        #         'id': q_meta['skill_cd'],
+        #         'name': q_meta['skill_desc'],
+        #         'count': 0,
+        #         'questions_id': []
+        #     }
+        #
+        # # Legacy code
+        # categories[q_meta['primary_class_cd']]['count'] += 1
+        # categories[q_meta['primary_class_cd']]['categories'][q_meta['skill_cd']]['count'] += 1
+        #
+        # module_count[q_meta['module']] += 1
+        #
+        # # for key in [key_module, key_module__difficulty, key]
+        #
+        # categories_questions_id[key_class].append([q_id, q_meta['questionId']])
+        # categories_counts[key_class] += 1
+        #
+        # if key_class not in categories_first_question_id:
+        #     categories_first_question_id[key_class] = q_meta['questionId']
+        #
+        # categories_questions_id[key_skill].append([q_id, q_meta['questionId']])
+        # categories_counts[key_skill] += 1
+        #
+        # if key_skill not in categories_first_question_id:
+        #     categories_first_question_id[key_skill] = q_meta['questionId']
 
         # categories[q_meta['primary_class_cd']]['questions_id'].append([q_id, q_meta['questionId']])
         # categories[q_meta['primary_class_cd']]['categories'][q_meta['skill_cd']]['questions_id'].append([q_id, q_meta['questionId']])
+        pass
 
 
-load_categories()
+# load_categories()
+
+
+@app.context_processor
+def utility_processor():
+    return dict(get_key=core.get_key, parse_key=core.parse_key,)
+
 
 @app.route("/")
 def index():
-    # redirect
-    return categories_page()
-    # return "<p>Hello, World!</p>"
+    return redirect("/categories")
 
 
-@app.route("/categories")
-def categories_page():
-    return render_template('categories.html', categories=categories, categories_first_question_id=categories_first_question_id, module_count=module_count)
+@app.route("/categories", defaults={'q_filter': ''})
+@app.route("/categories/<q_filter>/")
+def categories_page(q_filter):
+    categories = get_categories(q_filter)
+    module_count = {c['module']: c['module_count'] for c in categories}
+    module_first_question_id = {c['module']: c['module_first_question_id'] for c in categories}
+    filter_dict = core.parse_key(q_filter)
+    # print(module_count)
+    cat_get_key = partial(core.get_key, **filter_dict, module=None, only_key=['active', 'difficulty'])
+
+    return render_template(
+        'categories.html', 
+        categories=categories, 
+        # categories_first_question_id=questions_id(q_filter),
+        module_count=module_count,
+        module_first_question_id=module_first_question_id,
+        terms=terms,
+        filter_dict=filter_dict,
+        filter_s=q_filter,
+        cat_get_key=cat_get_key
+    )
 
 
-@app.route('/question/<question_id>')
-def question_page(question_id):
+# @app.route('/question/<question_id>')
+# def question_page(question_id):
+#     if question_id not in question_id_map:
+#         return "Question not found", 404
+# 
+#     question_id = question_id_map[question_id]
+#     question = questions[question_id]
+# 
+#     # questions_id: List[List[str, str]] = categories[question['primary_class_cd']]['categories'][question['skill_cd']]['questions_id']
+#     category = question['primary_class_cd'] + '-' + question['skill_cd']
+#     questions_id: List[List[str, str]] = categories_questions_id[category]
+# 
+#     question_index = [d[0] for d in questions_id].index(question_id)
+#     next_question_id = None
+#     if question_index < len(questions_id) - 1:
+#         next_question_id = questions_id[question_index + 1][1]
+# 
+#     questions_count = categories[question['primary_class_cd']]['categories'][question['skill_cd']]['count']
+#     current_question_number = question_index + 1
+# 
+#     return render_template('question.html',
+#        question=question,
+#        next_question_id=next_question_id,
+#        questions_count=questions_count,
+#        current_question_number=current_question_number,
+#        category=category
+#     )
+
+
+@app.route('/question/<q_filter>/<question_id>')
+def question_page(q_filter, question_id):
     if question_id not in question_id_map:
         return "Question not found", 404
+
+    filter_dict = core.parse_key(q_filter)
 
     question_id = question_id_map[question_id]
     question = questions[question_id]
 
-    # questions_id: List[List[str, str]] = categories[question['primary_class_cd']]['categories'][question['skill_cd']]['questions_id']
-    category = question['primary_class_cd'] + '-' + question['skill_cd']
-    questions_id: List[List[str, str]] = categories_questions_id[category]
+    questions_id: List[Tuple[str, str]] = get_questions_id(q_filter)
 
-    question_index = [d[0] for d in questions_id].index(question_id)
+    print(q_filter, question_id, len(questions_id), questions_id)
+
+    try:
+        question_index = [d[0] for d in questions_id].index(question_id)
+    except ValueError:
+        print('Question not found in filter', question_id, q_filter)
+        question_index = None
+
     next_question_id = None
-    if question_index < len(questions_id) - 1:
+    if question_index is not None and question_index < len(questions_id) - 1:
         next_question_id = questions_id[question_index + 1][1]
 
-    questions_count = categories[question['primary_class_cd']]['categories'][question['skill_cd']]['count']
-    current_question_number = question_index + 1
+    questions_count = get_questions_count(q_filter)
+    current_question_number = None
+    if question_index is not None and question_index >= 0:
+        current_question_number = question_index + 1
 
-    return render_template('question.html',
-       question=question,
-       next_question_id=next_question_id,
-       questions_count=questions_count,
-       current_question_number=current_question_number,
-       category=category
+    return render_template(
+        'question.html',
+        question=question,
+        next_question_id=next_question_id,
+        questions_count=questions_count,
+        current_question_number=current_question_number,
+        questions_id=[[i + 1, q[1], questions[q[0]]['difficulty']] for i, q in enumerate(questions_id)],
+        category='',  # TODO
+        terms=terms,
+        filter_dict=filter_dict,
+        filter_s=q_filter,
     )
 
 
@@ -121,7 +256,7 @@ def question_cat_page(category, question_id):
 
     questions_id: List[List[str, str]] = categories_questions_id[category]
 
-    print(questions_id)
+    # print(questions_id)
 
     question_index = [d[0] for d in questions_id].index(question_id)
     next_question_id = None
@@ -132,11 +267,12 @@ def question_cat_page(category, question_id):
     questions_count = categories_counts[category]
     current_question_number = question_index + 1
 
-    return render_template('question.html',
-       question=question,
-       next_question_id=next_question_id,
-       questions_count=questions_count,
-       current_question_number=current_question_number,
-       questions_id=[[i + 1, q[1], questions[q[0]]['difficulty']] for i, q in enumerate(questions_id)],
-       category=category,
+    return render_template(
+        'question.html',
+        question=question,
+        next_question_id=next_question_id,
+        questions_count=questions_count,
+        current_question_number=current_question_number,
+        questions_id=[[i + 1, q[1], questions[q[0]]['difficulty']] for i, q in enumerate(questions_id)],
+        category=category,
     )
---
+++ core.py
@@ -0,0 +1,280 @@
+import json
+from functools import lru_cache
+from pathlib import Path
+from typing import List, Optional, Dict, Any, Union, Tuple
+from peewee import SqliteDatabase, Model, CharField, IntegerField, BooleanField, fn
+import os
+
+lru_cache_maxsize = 1024
+
+
+# DB Connect
+db = SqliteDatabase('data/index.db')
+db.connect()
+
+
+class Question(Model):
+    q_id = CharField(unique=True)
+    questionId = CharField(unique=True)
+    skill_cd = CharField()
+    score_band_range_cd = IntegerField()
+    uId = CharField()
+    program = CharField()
+    external_id = CharField(null=True)
+    primary_class_cd = CharField()
+    difficulty = CharField()
+    module = CharField()
+    active = BooleanField()
+
+    class Meta:
+        database = db
+
+
+@lru_cache(maxsize=lru_cache_maxsize)
+def group_list():
+    return [q.primary_class_cd for q in Question.select(Question.primary_class_cd).distinct().order_by(Question.primary_class_cd)]
+
+
+@lru_cache(maxsize=lru_cache_maxsize)
+def skill_list():
+    return [q.skill_cd for q in Question.select(Question.skill_cd).distinct().order_by(Question.skill_cd)]
+
+
+def get_key(
+        module: str,
+        group: Optional[str] = None,
+        skill: Optional[str] = None,
+        difficulty: Optional[Union[str, List[str]]] = None,
+        active: Optional[str] = None,
+        only_key: Optional[List[str]] = None,
+        **kwargs
+) -> str:
+    # TODO rename primary_class_cd, skill_cd
+    if module is None:
+        module = 'all'
+
+    if module not in ['english', 'math']:
+        pass
+
+    if group is None:
+        group = 'all'
+
+    if skill is None:
+        skill = 'all'
+
+    if difficulty is None:
+        difficulty = 'all'
+
+    if type(difficulty) == str and difficulty != 'all':
+        difficulty = [difficulty]
+
+    if type(difficulty) == list:
+        difficulty_new = []
+        for diff in difficulty:
+            if diff in ['E', 'M', 'H']:
+                difficulty_new.append({'E': 'easy', 'M': 'medium', 'H': 'hard'}[diff])
+
+        difficulty = difficulty_new
+
+        difficulty = ','.join(difficulty)
+
+    if active is None:
+        active = 'all'
+    elif active in [0, 1]:
+        active = {0: 'non', 1: 'bluebook'}[active]
+
+    key_list = [
+        (module, 'module'),
+        (group, 'group'),
+        (skill, 'skill'),
+        (difficulty, 'difficulty'),
+        (active, 'active'),
+    ]
+    result = []
+    for key in key_list:
+        if only_key is not None and key[1] not in only_key:
+            continue
+        result.append(f"{key[1]}:{key[0]}")
+
+    return '-'.join(result)
+    # return f"module:{module}-class:{primary_class_cd}-skill:{skill_cd}-difficulty:{difficulty}-active:{active}"
+
+
+def parse_key(key: str) -> Dict[str, Any]:
+    filters_s = key.split('-')
+    filter_dict = {}
+
+    for filter_kv in filters_s:
+        if not filter_kv or ':' not in filter_kv:
+            continue
+
+        filter_k, filter_v = filter_kv.split(':')
+        if filter_k not in ['module', 'group', 'skill', 'difficulty', 'active']:
+            continue
+
+        if filter_k == 'module' and filter_v in ['english', 'math']:
+            filter_dict[filter_k] = filter_v
+
+        if filter_k == 'group' and filter_v in group_list():
+            filter_dict[filter_k] = filter_v
+
+        if filter_k == 'skill' and filter_v in skill_list():
+            filter_dict[filter_k] = filter_v
+
+        if filter_k == 'difficulty':
+            filter_dict[filter_k] = []
+            filter_v = filter_v.split(',')
+            for v in filter_v:
+                if v not in ['easy', 'medium', 'hard']:
+                    continue
+                filter_dict[filter_k].append({'easy': 'E', 'medium': 'M', 'hard': 'H'}[v])
+
+            if len(filter_dict[filter_k]) == 3:
+                filter_dict[filter_k] = 'all'
+            if len(filter_dict[filter_k]) == 0:
+                del filter_dict[filter_k]
+
+        # print(filter_k, filter_v)
+        if filter_k == 'active' and filter_v in ['bluebook', 'non']:
+            filter_dict[filter_k] = {'bluebook': 1, 'non': 0}[filter_v]
+
+    print('filter_dict', filter_dict)
+    return filter_dict
+
+
+def key_to_query_filter(key: str):
+    filter_dict = parse_key(key)
+
+    where = []
+    for k, v in filter_dict.items():
+        # print('key_to_query_filter', k, v)
+        if k == 'module':
+            where.append(Question.module == v)
+        if k == 'group':
+            where.append(Question.primary_class_cd == v)
+        if k == 'skill':
+            where.append(Question.skill_cd == v)
+        if k == 'difficulty':
+            if v == 'all':
+                continue
+            else:
+                where.append(Question.difficulty.in_(v))
+        if k == 'active':
+            # print('active', v)
+            where.append(Question.active == v)
+    # print('where', where)
+    return where
+
+
+def get_categories(key: str = '') -> List[Any]:
+    filter_dict = parse_key(key)
+    filter_dict = {k: v for k, v in filter_dict.items() if k in ['active', 'difficulty']}
+    key = get_key(module=None, **filter_dict)
+
+    # print('get_categories', key)
+
+    @lru_cache(maxsize=lru_cache_maxsize)
+    def get(key):
+        # print('get key', key)
+        where = key_to_query_filter(key) or [None]
+
+        question_module_counts = (
+            Question.select(
+                Question.module,
+                fn.COUNT().alias('count'),
+                Question.questionId.alias('first_question_id'),
+            )
+            .where(*where)
+            .order_by(Question.module)
+            .group_by(Question.module)
+        )
+
+        question_group_counts = (
+            Question.select(
+                Question.module,
+                Question.primary_class_cd,
+                fn.COUNT().alias('count'),
+                Question.questionId.alias('first_question_id')
+            )
+            .where(*where)
+            .order_by(Question.module, Question.primary_class_cd)
+            .group_by(Question.primary_class_cd)
+        )
+
+        question_skill_counts = (
+            Question.select(
+                Question.module,
+                Question.primary_class_cd,
+                Question.skill_cd,
+                fn.COUNT().alias('count'),
+                Question.questionId.alias('first_question_id')
+            )
+            .where(*where)
+            .order_by(Question.module, Question.primary_class_cd, Question.skill_cd)
+            .group_by(Question.primary_class_cd, Question.skill_cd)
+        )
+
+        # merge the results
+        question_module_counts = list(question_module_counts)
+        question_group_counts = list(question_group_counts)
+        question_skill_counts = list(question_skill_counts)
+        # combine the results side by side
+
+        # Loop through the results
+        cat_module = {}
+        cat_group = {}
+        cat_skill = {}
+        cats = []
+
+        for question in question_module_counts:
+            # print(f"Module: {question.module}, Count: {question.count}")
+            cat_module[question.module] = {
+                'count': question.count,
+                'first_question_id': question.first_question_id,
+            }
+
+        for question in question_group_counts:
+            # print(f"Module: {question.module}, Class: {question.primary_class_cd}, Count: {question.count}")
+            cat_group[question.primary_class_cd] = {
+                'count': question.count,
+                'first_question_id': question.first_question_id,
+            }
+
+        for question in question_skill_counts:
+            # print(f"Module: {question.module}, Class: {question.primary_class_cd}, Skill: {question.skill_cd}, Count: {question.count}")
+            cat_skill[question.skill_cd] = {
+                'count': question.count,
+                'first_question_id': question.first_question_id,
+            }
+            cats.append({
+                'module': question.module,
+                'module_count': cat_module[question.module]['count'],
+                'module_first_question_id': cat_module[question.module]['first_question_id'],
+
+                'group': question.primary_class_cd,
+                'group_count': cat_group[question.primary_class_cd]['count'],
+                'group_first_question_id': cat_group[question.primary_class_cd]['first_question_id'],
+
+                'skill': question.skill_cd,
+                'skill_count': cat_skill[question.skill_cd]['count'],
+                'skill_first_question_id': cat_skill[question.skill_cd]['first_question_id'],
+            })
+
+        return cats
+
+    return get(key)
+
+
+@lru_cache(maxsize=lru_cache_maxsize)
+def get_questions_id(filter_s: str = '') -> List[Tuple[str, str]]:
+    query = Question.select(Question.questionId, Question.uId).where(
+        *(key_to_query_filter(filter_s) or [None])
+    )
+    return [(q.uId, q.questionId) for q in query]
+
+
+@lru_cache(maxsize=lru_cache_maxsize)
+def get_questions_count(filter_s: str = '') -> int:
+    return Question.select(Question.questionId).where(
+        *(key_to_query_filter(filter_s) or [None])
+    ).count()
